0d8d927d6ef2806a457c16027d58e8d3fbebad9f
fernandogdo <fernandojaviergdo@gmail.com>
2024-07-23 19:50:20 -0500

    cambio nombre

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,25 +1,56 @@
-import { Button, Icon, Layout, Text } from '@ui-kitten/components';
-import { useAuthStore } from '../../store/auth/useAuthStore';
+import {useInfiniteQuery} from '@tanstack/react-query';
+import { NavigationProp, useNavigation } from '@react-navigation/native';
+import {getProductsByPage} from '../../../actions/products/get-products-by-page';
+import {MainLayout} from '../../layouts/MainLayout';
 
+import {FullScreenLoader} from '../../components/ui/FullScreenLoader';
+import {ProductList} from '../../components/products/ProductList';
+import { FAB } from '../../components/ui/FAB';
+import { RootStackParams } from '../../navigation/StackNavigator';
 
 export const HomeScreen = () => {
 
-  const { logout } = useAuthStore();
+  const navigation = useNavigation<NavigationProp<RootStackParams>>();
+
+  // const {isLoading, data: products = []} = useQuery({
+  //   queryKey: ['products', 'infinite'],
+  //   staleTime: 1000 * 60 * 60, // 1 hour
+  //   queryFn: () => getProductsByPage(0),
+  // });
+
+  const {isLoading, data, fetchNextPage} = useInfiniteQuery({
+    queryKey: ['products', 'infinite'],
+    staleTime: 1000 * 60 * 60, // 1 hour
+    initialPageParam: 0,
+
+    queryFn: async params => await getProductsByPage(params.pageParam),
+    getNextPageParam: (lastPage, allPages) => allPages.length,
+  });
 
   return (
-    <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>HomeScreen</Text>
+    <>
+      <MainLayout
+        title="TesloShop - Products"
+        subTitle="Aplicación administrativa">
+        {isLoading ? (
+          <FullScreenLoader />
+        ) : (
+          <ProductList
+            products={data?.pages.flat() ?? []}
+            fetchNextPage={fetchNextPage}
+          />
+        )}
+      </MainLayout>
 
-
-      {/* <Icon name="facebook" /> */}
-
-      <Button
-        accessoryLeft={ <Icon name="log-out-outline" /> }
-        onPress={ logout }
-      >
-        Cerrar sesión
-      </Button>
-      
-    </Layout>
-  )
-}
+      <FAB 
+        iconName="plus-outline"
+        onPress={() => navigation.navigate('ProductScreen',{ productId: 'new' })}
+        style={{
+          position: 'absolute',
+          bottom: 30,
+          right: 20,
+        }}
+      />
+    </>
+  );
+};
